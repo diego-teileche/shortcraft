f6fc04b040673fd36ffedaa200797c3a2cce1130
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-31 17:58:24 -0300

    Statistics Section | Elements styles added

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -17,12 +17,14 @@ const Statistic = () => {
 
 				<div className="statistics__cards">
 					<Card
+						className="brand"
 						image={BrandImg}
 						title="Brand Recognition"
 						description="Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content"
 						alt="Brand Recognition Image"
 					/>
 					<Card
+						className="detailed"
 						image={DetailedImg}
 						title="Detailed Records"
 						description="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decision"
