7a2174b7e26e8f76cc31ac39cc335382545b107b
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-31 19:56:41 -0300

    Client validation added

--- src/components/Shorten.tsx
+++ src/components/Shorten.tsx
@@ -1,12 +1,46 @@
+import { FormEvent, useState } from "react"
+
 const Shorten = () => {
+	const [url, setUrl] = useState("")
+	const [errorMessage, setErrorMessage] = useState("")
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault()
+
+		if (!url) {
+			console.error("No url")
+			setErrorMessage("Please add a link")
+		}
+
+		try {
+			const res = await fetch("/api/shortUrl", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: JSON.stringify({ url }),
+			})
+
+			console.log(res)
+		} catch (error) {
+			console.error("Error: ", error)
+		}
+	}
+
 	return (
 		<section className="shorten">
 			<div className="container">
 				<div className="shorten__content">
-					<form className="form">
+					<form onSubmit={handleSubmit} className="form">
 						<div className="input-control">
-							<input type="text" placeholder="Shorten a link here..." />
-							<p className="error-text">Please add a link</p>
+							<input
+								type="text"
+								value={url}
+								onChange={(e) => setUrl(e.target.value)}
+								className={`${errorMessage ? "error-input" : ""}`}
+								placeholder="Shorten a link here..."
+							/>
+							{errorMessage && <p className="error-text">{errorMessage}</p>}
 						</div>
 
 						<button className="btn" datatype="wide">
