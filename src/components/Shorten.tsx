36a2c9ae74c732ebc5f852f2580faa2fe38a81e1
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-09-01 14:46:21 -0300

    Displaying data and copy functionality added

--- src/components/Shorten.tsx
+++ src/components/Shorten.tsx
@@ -1,8 +1,16 @@
 import { FormEvent, useState } from "react"
 
+interface ShortenedUrlProps {
+	originalUrl: string
+	shortUrl: string
+}
+
 const Shorten = () => {
 	const [url, setUrl] = useState("")
+	const [shortenedUrls, setShortenedUrls] = useState<ShortenedUrlProps[]>([])
 	const [errorMessage, setErrorMessage] = useState("")
+	const [loading, setLoading] = useState(false)
+	const [copiedIndex, setCopiedIndex] = useState<number | null>(null)
 
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault()
@@ -13,6 +21,8 @@ const Shorten = () => {
 		}
 
 		try {
+			setLoading(true)
+
 			const res = await fetch("/api/shortUrl", {
 				method: "POST",
 				headers: {
@@ -22,10 +32,36 @@ const Shorten = () => {
 			})
 
 			const data = await res.json()
+
+			if (res.ok) {
+				setShortenedUrls([
+					...shortenedUrls,
+					{
+						originalUrl: url,
+						shortUrl: data.shortUrl,
+					},
+				])
+
+				setErrorMessage("")
+				setUrl("")
+			} else {
+				console.error("Error: ", data.error)
+			}
 			console.log(data)
 		} catch (error) {
 			console.error("Error: ", error)
+		} finally {
+			setLoading(false)
 		}
+	}
+
+	const handleCopy = (shortUrl: string, index: number) => {
+		navigator.clipboard.writeText(shortUrl)
+		setCopiedIndex(index)
+
+		setTimeout(() => {
+			setCopiedIndex(null)
+		}, 2000)
 	}
 
 	return (
@@ -44,30 +80,38 @@ const Shorten = () => {
 							{errorMessage && <p className="error-text">{errorMessage}</p>}
 						</div>
 
-						<button className="btn" datatype="wide">
-							Shorten it!
+						<button className="btn" datatype="wide" disabled={loading}>
+							{loading ? "Loading..." : "Shorten it!"}
 						</button>
 					</form>
 				</div>
 
-				<div className="shorten__cards">
-					<div className="shorten__card">
-						<div className="actual-link">
-							<span>https://www.diegoteileche.dev</span>
-						</div>
+				{shortenedUrls.length > 0 && (
+					<div className="shorten__cards">
+						{shortenedUrls.map((shortenedUrl, index) => (
+							<div key={index} className="shorten__card">
+								<div className="actual-link">
+									<span>{shortenedUrl.originalUrl}</span>
+								</div>
 
-						<hr className="line" />
+								<hr className="line" />
 
-						<div className="shorten__link">
-							<a href="#" target="_blank">
-								https://diee.te/5cH
-							</a>
-							<button className="btn" datatype="wide">
-								Copy
-							</button>
-						</div>
+								<div className="shorten__link">
+									<a href={`${shortenedUrl.shortUrl}`} target="_blank">
+										{shortenedUrl.shortUrl}
+									</a>
+									<button
+										className="btn"
+										datatype="wide"
+										onClick={() => handleCopy(shortenedUrl.shortUrl, index)}
+									>
+										{copiedIndex === index ? "Copied!" : "Copy"}
+									</button>
+								</div>
+							</div>
+						))}
 					</div>
-				</div>
+				)}
 			</div>
 		</section>
 	)
